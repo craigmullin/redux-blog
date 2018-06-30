a80e91660e8da0dbae2a1ba911044b674f35830e
Craig Mullin <cmullin@dreeshomes.com>
2018-06-30 07:37:58 -0400

    thru 123, Post Index

---
+++ src/components/post-list.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PostList extends Component {
+  render() {
+    return (
+      <div>Look at all these blog posts!</div>
+    );
+  }
+}
+
+export default PostList;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
-import App from './components/app';
+import { BrowserRouter, Route } from 'react-router-dom';
 import reducers from './reducers';
+import PostList from './components/post-list';
 
+// add two routes
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={PostList}/>
+      {/* <Route path="/posts/:id" component={PostDetail}/>
+      <Route path="/posts/new" component={PostCreate}/> */}
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
