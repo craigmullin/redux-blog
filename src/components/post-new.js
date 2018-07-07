449527c9bd3bcb6d1729ddcd866dc4be2dc09be0
Craig Mullin <cmullin@dreeshomes.com>
2018-07-07 09:07:43 -0400

    comment

--- src/components/post-new.js
+++ src/components/post-new.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Create a Post!</h3>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h3>Create a Post!</h3>
+        <form>
+          <Field
+            name="title" >
+            {/* component={} > */}
+          </Field>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default PostNew;
+export default reduxForm({
+    form: 'PostNewForm'
+}) (PostNew);
